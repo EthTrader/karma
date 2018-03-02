bdc5219b83d1691722adf4095f48ba7f6271fe85
Carl Larson <cslarson@gmail.com>
2018-03-02 13:30:56 +0000

    fix paste error

--- copy_users.sql
+++ copy_users.sql
@@ -1,5 +1,5 @@
 COPY (WITH
-  first_content AS (SELECT DISTINCT ON(author) author, extract(epoch from reddit_created_utc) AS firstConten$
+  first_content AS (SELECT DISTINCT ON(author) author, extract(epoch from reddit_created_utc) AS firstContent FROM content ORDER BY author, reddit_created_utc ASC),
   scores AS (SELECT author, sum(score) as score FROM content GROUP BY author)
 SELECT username, address, score, firstContent
 FROM users
